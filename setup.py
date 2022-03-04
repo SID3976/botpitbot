f5269d741ab5da1fe510b71f2271df8335b76e9c
SID <100883480+SID3976@users.noreply.github.com>
2022-03-04 03:44:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="Botpitbot",
+    packages=["Botpitbot"],
+    entry_points={
+        "console_scripts": [
+            "bot = bot.tele_bot:main",
+            ],
+        },
+    )
